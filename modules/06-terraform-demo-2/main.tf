81ec6b4291d7d75b64a2e996b0d0b07110f4e0e7
Jan Erik Johansen <jan.erik.johansen@bspoke.no>
2022-02-15 19:28:48 +0100

    tag

--- modules/06-terraform-demo-2/main.tf
+++ modules/06-terraform-demo-2/main.tf
@@ -21,8 +21,13 @@ resource "azurerm_resource_group" "rg" {
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = var.location
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = "Terraform Demo"
+    Team        = "DevOps"
+  }
 }
